3c3ef1938720139aec6db191c42822d4433fb907
Mugagga <Mugagga.kimera04@gmail.com>
2025-10-19 04:32:23 +0100

    added some comments to the interpreter for better understanding

--- src/FSharpLib/Interpreter.fs
+++ src/FSharpLib/Interpreter.fs
@@ -3,115 +3,117 @@
 // Date: 23/10/2022
 // Reference: Peter Sestoft, Grammars and parsing with F#, Tech. Report
 
-open System
 
-type terminal = 
-    Add | Sub | Mul | Div | Lpar | Rpar | Num of int
+module Interpreter
+    open System
+    type terminal = 
+        Add | Sub | Mul | Div | Lpar | Rpar | Num of int
 
-let str2lst s = [for c in s -> c]
-let isblank c = System.Char.IsWhiteSpace c
-let isdigit c = System.Char.IsDigit c
-let lexError = System.Exception("Lexer error")
-let intVal (c:char) = (int)((int)c - (int)'0')
-let parseError = System.Exception("Parser error")
+    let str2lst s = [for c in s -> c] // simple function to convert string to list of characters
+    let isblank c = System.Char.IsWhiteSpace c // checks if is blank
+    let isdigit c = System.Char.IsDigit c // checks if its a number (obviously)
+    let lexError = System.Exception("Lexer error") // error declaration 
+    let intVal (c:char) = (int)((int)c - (int)'0') // fast way to turn string to number  - number representation of number minus acsii number representation of 0 
+    let parseError = System.Exception("Parser error") // error declaration
 
-let rec scInt(iStr, iVal) = 
-    match iStr with
-    c :: tail when isdigit c -> scInt(tail, 10*iVal+(intVal c))
-    | _ -> (iStr, iVal)
+    let rec scInt(iStr, iVal) =  // recursive function to scan integer values
+        match iStr with
+        c :: tail when isdigit c -> scInt(tail, 10*iVal+(intVal c)) // if digit then recursively call function , passing the rest of the number string and updating the integer value accordingly
+        | _ -> (iStr, iVal)
 
-let lexer input = 
-    let rec scan input =
-        match input with
-        | [] -> []
-        | '+'::tail -> Add :: scan tail
-        | '-'::tail -> Sub :: scan tail
-        | '*'::tail -> Mul :: scan tail
-        | '/'::tail -> Div :: scan tail
-        | '('::tail -> Lpar:: scan tail
-        | ')'::tail -> Rpar:: scan tail
-        | c :: tail when isblank c -> scan tail
-        | c :: tail when isdigit c -> let (iStr, iVal) = scInt(tail, intVal c) 
-                                      Num iVal :: scan iStr
-        | _ -> raise lexError
-    scan (str2lst input)
+    let lexer input = 
+        let rec scan input = // recursive function (rec keyword indicates its recursive)
+            match input with // match with the following
+            | [] -> [] // empty array - add nothing 
+            | '+'::tail -> Add :: scan tail // if plus then  -> append "Add" operator to array and call scan function with the rest of the array 
+            | '-'::tail -> Sub :: scan tail // same as above for subtraction
+            | '*'::tail -> Mul :: scan tail // same as above for multiplication
+            | '/'::tail -> Div :: scan tail // same as above for division
+            | '('::tail -> Lpar:: scan tail // same as above for left parenthesis
+            | ')'::tail -> Rpar:: scan tail // same as above for right parenthesis
+            | c :: tail when isblank c -> scan tail // if blank space then just call scan on the rest of the array
+            | c :: tail when isdigit c -> let (iStr, iVal) = scInt(tail, intVal c) // if digit then call scInt function to get the full number (in case of multiple digits)
+                                          Num iVal :: scan iStr // append Num with the value to the array and call scan on the rest of the array
+            | _ -> raise lexError // raise lexer error if none of the above match
+        scan (str2lst input) // call scan function on the input string converted to a list of characters
 
-let getInputString() : string = 
-    Console.Write("Enter an expression: ")
-    Console.ReadLine()
+    let getInputString() : string = 
+        Console.Write("Enter an expression: ")
+        Console.ReadLine()
 
-// Grammar in BNF:
-// <E>        ::= <T> <Eopt>
-// <Eopt>     ::= "+" <T> <Eopt> | "-" <T> <Eopt> | <empty>
-// <T>        ::= <NR> <Topt>
-// <Topt>     ::= "*" <NR> <Topt> | "/" <NR> <Topt> | <empty>
-// <NR>       ::= "Num" <value> | "(" <E> ")"
+    // Grammar in BNF:
+    // <E>        ::= <T> <Eopt>
+    // <Eopt>     ::= "+" <T> <Eopt> | "-" <T> <Eopt> | <empty>
+    // <T>        ::= <NR> <Topt>
+    // <Topt>     ::= "*" <NR> <Topt> | "/" <NR> <Topt> | <empty>
+    // <NR>       ::= "Num" <value> | "(" <E> ")"
 
-let parser tList = 
-    let rec E tList = (T >> Eopt) tList         // >> is forward function composition operator: let inline (>>) f g x = g(f x)
-    and Eopt tList = 
-        match tList with
-        | Add :: tail -> (T >> Eopt) tail
-        | Sub :: tail -> (T >> Eopt) tail
-        | _ -> tList
-    and T tList = (NR >> Topt) tList
-    and Topt tList =
-        match tList with
-        | Mul :: tail -> (NR >> Topt) tail
-        | Div :: tail -> (NR >> Topt) tail
-        | _ -> tList
-    and NR tList =
-        match tList with 
-        | Num value :: tail -> tail
-        | Lpar :: tail -> match E tail with 
-                          | Rpar :: tail -> tail
-                          | _ -> raise parseError
-        | _ -> raise parseError
-    E tList
+    let parser tList =  // recursive descent parser implementation -- works in BIDMAS order from bottom up 
+        let rec E tList = (T >> Eopt) tList         // >> is forward function composition operator: let inline (>>) f g x = g(f(x)) // performs a recursive descent parse
+        and Eopt tList =  // if addition or subtraction found then call T on the rest of the list and then Eopt again recursively
+            match tList with
+            | Add :: tail -> (T >> Eopt) tail
+            | Sub :: tail -> (T >> Eopt) tail
+            | _ -> tList
+        and T tList = (NR >> Topt) tList
+        and Topt tList = // if division or multiplication found then call NR on the rest of the list and then Topt again recursively
+            match tList with
+            | Mul :: tail -> (NR >> Topt) tail
+            | Div :: tail -> (NR >> Topt) tail
+            | _ -> tList
+        and NR tList = // works from the bottom up -- checks for numbers and parentheses
+            match tList with 
+            | Num value :: tail -> tail // if number found then return the rest of the list (since its recursive , the returned will be passed up the chain)
+            | Lpar :: tail -> match E tail with  // if left parenthesis found then call E on the rest of the list to find the matching right parenthesis
+                              | Rpar :: tail -> tail
+                              | _ -> raise parseError // raises error if no matching right parenthesis found
+            | _ -> raise parseError
+        E tList
 
-let parseNeval tList = 
-    let rec E tList = (T >> Eopt) tList
-    and Eopt (tList, value) = 
-        match tList with
-        | Add :: tail -> let (tLst, tval) = T tail
-                         Eopt (tLst, value + tval)
-        | Sub :: tail -> let (tLst, tval) = T tail
-                         Eopt (tLst, value - tval)
-        | _ -> (tList, value)
-    and T tList = (NR >> Topt) tList
-    and Topt (tList, value) =
-        match tList with
-        | Mul :: tail -> let (tLst, tval) = NR tail
-                         Topt (tLst, value * tval)
-        | Div :: tail -> let (tLst, tval) = NR tail
-                         Topt (tLst, value / tval)
-        | _ -> (tList, value)
-    and NR tList =
-        match tList with 
-        | Num value :: tail -> (tail, value)
-        | Lpar :: tail -> let (tLst, tval) = E tail
-                          match tLst with 
-                          | Rpar :: tail -> (tail, tval)
-                          | _ -> raise parseError
-        | _ -> raise parseError
-    E tList
+    let parseNeval tList = 
+        let rec E tList = (T >> Eopt) tList
+        and Eopt (tList, value) =  // takes a operator and a value
+            match tList with
+            | Add :: tail -> let (tLst, tval) = T tail // if opperator is addition then call T on the rest of the list which goes to the bottom until a number is found 
+                             Eopt (tLst, value + tval) // then recursively call Eopt again with the updated value
+            | Sub :: tail -> let (tLst, tval) = T tail
+                             Eopt (tLst, value - tval)
+            | _ -> (tList, value) // if no operator found then return the list and the value
+        and T tList = (NR >> Topt) tList
+        and Topt (tList, value) =
+            match tList with
+            | Mul :: tail -> let (tLst, tval) = NR tail
+                             Topt (tLst, value * tval)
+            | Div :: tail -> let (tLst, tval) = NR tail
+                             Topt (tLst, value / tval)
+            | _ -> (tList, value)
+        and NR tList = // works from the bottom up  // the actual evaluation starts here then gets returned up the chain
+            match tList with 
+            | Num value :: tail -> (tail, value)
+            | Lpar :: tail -> let (tLst, tval) = E tail // in the case of a left parenthesis
+                              match tLst with 
+                              | Rpar :: tail -> (tail, tval) // finds right one 
+                              | _ -> raise parseError // raises error otherwise 
+            | _ -> raise parseError
+        E tList
 
-let rec printTList (lst:list<terminal>) : list<string> = 
-    match lst with
-    head::tail -> Console.Write("{0} ",head.ToString())
-                  printTList tail
-                  
-    | [] -> Console.Write("EOL\n")
-            []
+    let rec printTList (lst:list<terminal>) : list<string> = 
+        match lst with
+        head::tail -> Console.Write("{0} ",head.ToString())
+                      printTList tail
+                      
+        | [] -> Console.Write("EOL\n")
+                []
 
 
-[<EntryPoint>]
-let main argv  =
-    Console.WriteLine("Simple Interpreter")
-    let input:string = getInputString()
-    let oList = lexer input
-    let sList = printTList oList;
-    let pList = printTList (parser oList)
-    let Out = parseNeval oList
-    Console.WriteLine("Result = {0}", snd Out)
-    0
+// [<EntryPoint>]
+// let main argv  =
+//     Console.WriteLine("Simple Interpreter")
+//     let input:string = getInputString()
+//     let oList = lexer input // run lexer function 
+//     let sList = printTList oList; // print the list 
+//     let pList = printTList (parser oList)
+//     let Out = parseNeval oList
+//     Console.WriteLine()
+//     Console.WriteLine("Result = {0}", snd Out)
+//     0
